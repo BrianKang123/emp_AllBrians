05432983d6eef2ac83311539b091342f9f7029c8
brianwang30 <bwang30@stuy.edu>
2021-12-17 12:26:14 -0500

    linsearch

---
+++ LinSearch.java
@@ -0,0 +1,38 @@
+public class LinSearch
+{
+
+  /**
+     int linSearch(Comparable[],Comparable) -- searches an array of
+     Comparables for target
+     post: returns index of first occurrence of target, or
+     returns -1 if target not found
+  **/
+  public static int linSearch ( Comparable[] a, Comparable target )
+  {
+    int tPos = -1;
+    int i = 0;
+
+    while ( i < a.length ) {
+      if ( a[i].equals(target) ) {
+        tPos = i;
+        break;
+      }
+      i++;
+    }
+    return tPos;
+  }
+
+
+  // utility/helper fxn to display contents of an array of Objects
+  private static void printArray( Object[] arr )
+  {
+    String output = "[ ";
+
+    for( Object o : arr )
+      output += o + ", ";
+
+    output = output.substring( 0, output.length()-2 ) + " ]";
+
+    System.out.println( output );
+  }
+}
