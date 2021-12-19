5b867fe029fa3eaaf6a104b14f8bbba183407230
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2021-12-18 22:15:51 -0500

    complete driver

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,31 +1,81 @@
-import java.util.ArrayList;
-
 public class SearchDriver {
     public static void main(String[] args) {
-        // initialize and fill ArrayList of len 10000 from 0 - 9999
-        ArrayList<Integer> arr10000 = new ArrayList<Integer>();
-        for (int i = 0; i < 10000; i++) {
-            arr10000.add(i);
+        int arrLength = 100_000_000;
+
+        // update arrLength if provided through args
+        if (args.length != 0) {
+            arrLength = Integer.parseInt(args[0]);
+            System.out.println(arrLength);
         }
 
-        for (int i = 0; i < 10; i++) {
-            long start = System.currentTimeMillis();
-            // perform binsearch
-            long stop = System.currentTimeMillis();
-            long diff = stop - start;
-            System.out.println("start: " + start);
-            System.out.println("stop:  " + stop);
-            System.out.println("time elapsed: " + diff);
+        // initialize and fill array of Comparables from 0 - arrLength - 1
+        Comparable[] arr = new Comparable[arrLength];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
         }
 
-        for (int i = 0; i < 10; i++) {
-            long start = System.currentTimeMillis();
-            // perform linsearch
-            long stop = System.currentTimeMillis();
-            long diff = stop - start;
-            System.out.println("start: " + start);
-            System.out.println("stop:  " + stop);
-            System.out.println("time elapsed: " + diff);
+        // initialize stats
+        long minTime, maxTime, totalTime;
+        int maxTimeIdx, minTimeIdx;
+        maxTime = 0;
+        minTime = 999999999;
+        totalTime = 0;
+        maxTimeIdx = -1;
+        minTimeIdx = -1;
+
+        // perform 1000 binary searches
+        for (int i = 0; i < 1000; i++) {
+            int randInt = (int) (Math.random() * arrLength);
+            long start = System.currentTimeMillis();      // record start time
+            BinSearch.binSearch(arr, randInt);
+            long stop = System.currentTimeMillis();       // record stop time
+            long diff = stop - start;                     // calc time elapsed
+            totalTime += diff;
+            if (diff > maxTime) {
+                maxTime = diff;
+                maxTimeIdx = randInt;
+            }
+            if (diff < minTime) {
+                minTime = diff;
+                minTimeIdx = randInt;
+            }
         }
+        System.out.println("results from running 1,000 binary searches on an " +
+                           "array of length " + arrLength +
+                           "\naverage:   " + totalTime / 1000.0 + " ms" +
+                           "\nminTime:   " + minTime + " ms\t\tidx:   " + minTimeIdx +
+                           "\nmaxTime:   " + maxTime + " ms\t\tidx:   " + maxTimeIdx +
+                           "\ntotalTime: " + totalTime + " ms");
+
+        // reset stats
+        maxTime = 0;
+        minTime = 999999999;
+        totalTime = 0;
+        maxTimeIdx = -1;
+        minTimeIdx = -1;
+
+        // perform 1000 linear searches
+        for (int i = 0; i < 1000; i++) {                  // perform 1000 trials
+            int randInt = (int) (Math.random() * arrLength);
+            long start = System.currentTimeMillis();      // record start time
+            LinSearch.linSearch(arr, randInt);
+            long stop = System.currentTimeMillis();       // record stop time
+            long diff = stop - start;                     // calc time elapsed
+            totalTime += diff;
+            if (diff > maxTime) {
+                maxTime = diff;
+                maxTimeIdx = randInt;
+            }
+            if (diff < minTime) {
+                minTime = diff;
+                minTimeIdx = randInt;
+            }
+        }
+        System.out.println("results from running 1,000 linear searches on an " +
+                           "array of length " + arrLength +
+                           "\naverage:   " + totalTime / 1000.0 + " ms" +
+                           "\nminTime:   " + minTime + " ms\t\tidx:   " + minTimeIdx +
+                           "\nmaxTime:   " + maxTime + " ms\tidx:   " + maxTimeIdx +
+                           "\ntotalTime: " + totalTime + " ms");
     }
 }
